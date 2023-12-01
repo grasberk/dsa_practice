39c9a560462b676d5d96dd69cf33107d15c4bef1
grasberk <ganesh.avula123@gmail.com>
2023-12-01 13:33:15 -0600

    summarize bs probelm

---
+++ binary_search.py
@@ -0,0 +1,9 @@
+
+# S: a list of number are given from 0-n, find the number that is not given in the fitted range
+# E
+# A
+# T
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        pass

c6f9ad27161cb4c95302768778f9703b8016397b
grasberk <ganesh.avula123@gmail.com>
2023-12-07 14:05:53 -0600

    examples

--- reverse_linklist.py
+++ reverse_linklist.py
@@ -1,6 +1,15 @@
 
 # S: given a list of numbers return a list with the numbers in reverse order from the original
 # E:
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+# Input: head = [1,2]
+# Output: [2,1]
+# Example 3:
+# Input: head = []
+# Output: []
+# input: head=[-3,-2,-1,0,1,2,3]
+# output=[3,2,1,0,-1,-2,-3]
 # A:
 # T:
 

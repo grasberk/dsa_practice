af316669640f7bfd173a88d2a91b247cae4f1fe5
grasberk <ganesh.avula123@gmail.com>
2023-12-07 14:03:01 -0600

    summery

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,35 @@
 
 # S: a list of number are given from 0-n, find the number that is not given in the fitted range
-# E
+# E: Example 1:
+
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: 2
+# Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+# Example 3:
+
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
 # A
 # T
-
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        pass
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high)//2
+
+            if (target == nums[mid]):
+                return mid
+            elif (target > nums[mid]):
+                low = mid + 1
+            else:                      
+                high = mid - 1
+
+        return -1
---
+++ reverse_linklist.py
@@ -0,0 +1,14 @@
+
+# S: given a list of numbers return a list with the numbers in reverse order from the original
+# E:
+# A:
+# T:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
